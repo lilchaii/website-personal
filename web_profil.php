299bdae39ad56b4203537e3da0f9c653f5c3836d
lilichaii <140490978+lilchaii@users.noreply.github.com>
2023-07-25 14:57:21 +0700

    Add files via upload

---
+++ web_profil.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lili Personal Website</title>
+</head>
+
+<body>
+    <br>
+    <header style="text-align: center;">
+        <img src="profil.jpg"  width="200" height="200" style="border-radius: 50%;"/>
+        <h1>Lili</h1>
+        <p>(Junior Web Developer)</p>
+    </header>
+
+    <hr />
+
+    <article style="text-align: center;">
+        <h2>Overview</h2>
+        <p>
+            Halo, saya adalah junior web developer yang berdomisili di Tebas.
+            Saat ini sedang belajar mengenai dunia web developer. 
+            Web developer bertanggung jawab atas pembuatan, pembangunan, dan pemeliharaan website. 
+            Tidak hanya merancang tampilan website, tetapi juga memastikan seluruh fungsi berjalan tanpa kendala.
+        </p>
+    </article>
+
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>Skill</th>
+                    <th>Pengalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML (Expert)</li>
+                            <li>CSS (Beginner)</li>
+                            <li>Javascript (Beginner)</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Freelancer di Internet</li>
+                            <li>Pembuatan Web Sederhana</li>
+                            <li>Project Pelatihan Web Developer</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright &copy; 2022 Lili.</p>
+    </footer>
+</body>
+</html>
